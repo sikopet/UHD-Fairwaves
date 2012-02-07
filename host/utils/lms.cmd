16f23ec1e4b0628309b42389e10d2c71e8414415
Alexander Chemeris <Alexander.Chemeris@gmail.com>
2012-02-07 22:06:39 +0400

    HOST: Modify usrp_n2xx_net_burner.py to support UmTRX.

---
+++ host/utils/lms.cmd
@@ -0,0 +1,9 @@
+D:\Python27\python.exe umtrx_lms.py --lms 1 --lms-init --umtrx-addr 192.168.0.102
+D:\Python27\python.exe umtrx_lms.py --lms 1 --lms-tx-enable 1 --umtrx-addr 192.168.0.102
+D:\Python27\python.exe umtrx_lms.py --lms 1 --lms-rx-enable 1 --umtrx-addr 192.168.0.102
+D:\Python27\python.exe umtrx_lms.py --lms 1 --lms-set-tx-vga1-gain -10
+D:\Python27\python.exe umtrx_lms.py --lms 1 --pll-ref-clock 26e6 --lpf-bandwidth-code 0x0f --lms-auto-calibration --umtrx-addr 192.168.0.102
+D:\Python27\python.exe umtrx_lms.py --lms 1 --lms-tx-pll-tune 925e6 --umtrx-addr 192.168.0.102
+
+
+
---
+++ host/utils/lms2.cmd
@@ -0,0 +1,5 @@
+D:\Python27\python.exe umtrx_lms.py --lms 1 --lms-init
+D:\Python27\python.exe umtrx_lms.py --lms 1 --lms-tx-enable 1
+D:\Python27\python.exe umtrx_lms.py --lms 1 --lms-rx-enable 1
+D:\Python27\python.exe umtrx_lms.py --lms 1 --pll-ref-clock 26e6 --lpf-bandwidth-code 0x0f --lms-auto-calibration
+
