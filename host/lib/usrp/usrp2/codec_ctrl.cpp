a219498b6b4e156742fd74f86f20a1fc60db860c
Zabbal <suarev@stud.ntnu.no>
2012-02-12 13:52:16 +0400

    HOST: revert controllers to original state.

--- host/lib/usrp/usrp2/codec_ctrl.cpp
+++ host/lib/usrp/usrp2/codec_ctrl.cpp
@@ -94,7 +94,7 @@ public:
             this->send_ads62p44_reg(0x14);
             this->set_rx_analog_gain(1);
             break;
-	case usrp2_iface::UMTRX_REV0:
+
         case usrp2_iface::USRP_NXXX: break;
         }
     }
@@ -119,7 +119,7 @@ public:
             _ads62p44_regs.power_down = ads62p44_regs_t::POWER_DOWN_GLOBAL_PD;
             this->send_ads62p44_reg(0x14);
             break;
-	case usrp2_iface::UMTRX_REV0:
+
         case usrp2_iface::USRP_NXXX: break;
         }
     )}
@@ -146,7 +146,6 @@ public:
 
     void set_rx_digital_gain(double gain) {  //fine digital gain
         switch(_iface->get_rev()){
-        case usrp2_iface::UMTRX_REV0:
         case usrp2_iface::USRP_N200:
         case usrp2_iface::USRP_N210:
         case usrp2_iface::USRP_N200_R4:
@@ -161,7 +160,6 @@ public:
 
     void set_rx_digital_fine_gain(double gain) { //gain correction      
         switch(_iface->get_rev()){
-        case usrp2_iface::UMTRX_REV0:
         case usrp2_iface::USRP_N200:
         case usrp2_iface::USRP_N210:
         case usrp2_iface::USRP_N200_R4:
@@ -176,7 +174,6 @@ public:
 
     void set_rx_analog_gain(bool /*gain*/) { //turns on/off analog 3.5dB preamp
         switch(_iface->get_rev()){
-        case usrp2_iface::UMTRX_REV0:
         case usrp2_iface::USRP_N200:
         case usrp2_iface::USRP_N210:
         case usrp2_iface::USRP_N200_R4:
@@ -196,24 +193,20 @@ private:
     uhd::spi_iface::sptr _spiface;
 
     void send_ad9777_reg(boost::uint8_t addr){
-        if (usrp2_iface::UMTRX_REV0 != _iface->get_rev()) {
-            boost::uint16_t reg = _ad9777_regs.get_write_reg(addr);
-            UHD_LOGV(always) << "send_ad9777_reg: " << std::hex << reg << std::endl;
-            _spiface->write_spi(
-                SPI_SS_AD9777, spi_config_t::EDGE_RISE,
-                reg, 16
-            );
-        }
+        boost::uint16_t reg = _ad9777_regs.get_write_reg(addr);
+        UHD_LOGV(always) << "send_ad9777_reg: " << std::hex << reg << std::endl;
+        _spiface->write_spi(
+            SPI_SS_AD9777, spi_config_t::EDGE_RISE,
+            reg, 16
+        );
     }
 
     void send_ads62p44_reg(boost::uint8_t addr) {
-        if (usrp2_iface::UMTRX_REV0 != _iface->get_rev()) {
-            boost::uint16_t reg = _ads62p44_regs.get_write_reg(addr);
-            _spiface->write_spi(
-                SPI_SS_ADS62P44, spi_config_t::EDGE_FALL,
-                reg, 16
-            );
-        }
+        boost::uint16_t reg = _ads62p44_regs.get_write_reg(addr);
+        _spiface->write_spi(
+            SPI_SS_ADS62P44, spi_config_t::EDGE_FALL,
+            reg, 16
+        );
     }
 };
 
